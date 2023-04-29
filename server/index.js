bc46ff5ebc32696e9d602dcc4a70d4323eb5a10f
hr0065 <hsr21sinha@gmail.com>
2023-04-29 23:07:34 +0530

    update

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@
     app.use(bodyParser.json({ limit: "30mb", extended: true }));
     app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
     app.use(cors());
-    app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+    app.use("/assets", express.static("public/assets"));
 
     /* FILE STORAGE */
     const storage = multer.diskStorage({
